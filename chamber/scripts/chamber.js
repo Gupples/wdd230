cba1d3049f3b78ce150369844a8fcf369c8e29e2
Gupples <85248061+Gupples@users.noreply.github.com>
2024-10-12 19:29:14 -0600

    Initial push for Chamber

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,14 @@
+// Replace the current year.
+const options = {
+    year: "numeric"
+}
+
+const footer = document.querySelector('p');
+let footerText = footer.textContent;
+const currentDate = new Date().toLocaleDateString("en-US", options);
+let newFooter = footerText.replace('CurrentYear', currentDate)
+footer.textContent = newFooter;
+
+// Insert last modification information
+const modified = document.lastModified;
+document.querySelector('#lastModified').textContent = `Last Modification: ${modified}`;
