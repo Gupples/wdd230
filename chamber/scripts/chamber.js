35631dc997135a7dae932bca092b0a717b46a1bb
Gupples <85248061+Gupples@users.noreply.github.com>
2024-10-12 22:13:03 -0600

    Finished main page (phone view)

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,3 +1,32 @@
+// BUTTONS
+// Hamburgur Button
+const hamButton = document.querySelector('#menuButton');
+const spacer = document.querySelector('#spacer');
+const navLinks = document.querySelector('#navLinks');
+
+hamButton.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+    hamButton.classList.toggle('open');
+    
+    if (navLinks.classList.contains('open'))
+        {
+            spacer.classList.remove('open');
+        } else {
+            spacer.classList.add('open')
+        };
+        
+        
+    });
+
+// Dark Mode Button
+const main = document.querySelector('main');
+const darkButton = document.querySelector('#darkModeButton')
+darkButton.addEventListener('click', () => {
+    main.classList.toggle('dark');
+    darkButton.classList.toggle('dark');
+});
+
+// DATES
 // Replace the current year.
 const options = {
     year: "numeric"
